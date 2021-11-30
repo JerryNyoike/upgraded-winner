8018495b4771bf088f0f74ff5dccf27953b57e54
pancakem <pancakesdeath@protonmail.com>
2021-11-30 20:03:44 +0300

    Move type information to types module

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,61 @@
-use crate::type_checker::*;
+use std::collections::HashMap;
+use std::fmt;
+use std::string::ToString;
+
+pub type Ident = String;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum MirandaType {
+    Bool,
+    Int,
+    Float,
+    List(Box<MirandaType>),
+    Char,
+    String,
+}
+
+impl ToString for MirandaType {
+    fn to_string(&self) -> String {
+        match self {
+            MirandaType::Bool => "bool".to_string(),
+            MirandaType::Int => "int".to_string(),
+            MirandaType::Float => "float".to_string(),
+            MirandaType::Char => "char".to_string(),
+            MirandaType::String => "string".to_string(),
+            MirandaType::List(x) => "list ".to_string() + &(*x).to_string(),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct VarType(pub Ident, pub MirandaType);
+
+impl VarType {
+    pub fn new(id: Ident, t: MirandaType) -> Self {
+        Self(id, t)
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct FunType(pub Ident, pub Vec<MirandaType>);
+
+impl FunType {
+    pub fn new(id: Ident, t: Vec<MirandaType>) -> Self {
+        Self(id, t)
+    }
+}
+
+impl fmt::Display for FunType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for v in &self.1 {
+            write!(f, "{} ", v.to_string())?;
+        }
+        Ok(())
+    }
+}
+
+pub type FunTable = HashMap<Ident, FunType>;
+pub type VarTable = HashMap<Ident, VarType>;
 
 // Supported types for this Miranda
 #[derive(Debug, PartialEq, Clone)]
@@ -51,26 +108,62 @@ pub struct Env {
 }
 
 impl Env {
+    pub fn function_lookup(&self, identifier: &Ident) -> Option<FunType> {
+        if let Some(x) = self.funs_table.get(identifier) {
+            return Some((*x).clone());
+        }
+        return None;
+    }
+
+    pub fn variable_lookup(&self, identifier: &Ident) -> Option<VarType> {
+        if let Some(x) = self.vars_table.get(identifier) {
+            return Some((*x).clone());
+        }
+        return None;
+    }
+
+    // insert a variable to table
     fn extend_var(&mut self, id: Ident, t: MirandaType) {
-        if let None = self.vars_table.get(&id) {
-            // Type Ident already defined
-            println!("Variable {} {} already defined", t.to_string(), id);
+        if let Some(v) = self.variable_lookup(&id) {
+            println!("Variable {} {} already defined", v.0, v.1.to_string());
             return;
         }
         self.vars_table.insert(id.clone(), VarType::new(id, t));
     }
 
+    // insert a function to the table
     fn extend_fn(&mut self, id: Ident, t: Vec<MirandaType>) {
-        if let None = self.funs_table.get(&id) {
-            // Type Ident already defined
-            let fun = FunType::new(id, t);
-            println!("{}", fun);
+        if let Some(f) = self.function_lookup(&id) {
+            println!("Function {} is already defined", f);
             return;
         }
         self.funs_table.insert(id.clone(), FunType::new(id, t));
     }
 
-    fn check(&self, t: MirandaType) {}
+    // returns false if the name is not found in the symbol table
+    // true otherwise
+    fn name_lookup(&self, id: Ident) -> bool {
+        if let None = self.variable_lookup(&id) {
+            if let None = self.function_lookup(&id) {
+                return false;
+            }
+        }
+        true
+    }
+
+    // checks if variable identifier has specified type
+    fn check_var(&self, id: Ident, t: MirandaType) -> bool {
+        // if let Some(v) = variable_lookup()
+        false
+    }
 }
 
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn type_string() {
+        let value: MirandaType = MirandaType::List(Box::new(MirandaType::Int));
+        assert_eq!(value.to_string(), "list int")
+    }
+}
